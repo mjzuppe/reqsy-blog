bc6b2cc111da3818b435140aef5c358cfbc36a35
Mark Zuppe <zuppe@live.com>
2023-11-14 11:57:56 -0500

    init

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Product People';
+export const SITE_DESCRIPTION = 'Product People is a community of product managers, designers, and developers who build and manage digital products. Powered by Reqsy.';
